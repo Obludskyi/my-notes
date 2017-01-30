bcc9754e2153313c104aa2c99bda1fb95de89931
Alex Obludsky <Obludsky@ukr.net>
2017-01-31 00:30:28 +0200

    Configured webpack and library.

---
+++ client/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Oleksii on 30.01.2017.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h1>Notes</h1>,
+    document.getElementById('mount-point')
+);
