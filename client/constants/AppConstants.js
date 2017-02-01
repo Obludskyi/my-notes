3ec93953f1ca84e589b6929ce4171fca51866955
Alex Obludsky <Obludsky@ukr.net>
2017-02-01 22:03:32 +0200

    Add constants.

---
+++ client/constants/AppConstants.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Oleksii on 01.02.2017.
+ */
+import keyMirror from 'keymirror';
+
+export default keyMirror({
+    LOAD_NOTES_REQUEST: null,
+    LOAD_NOTES_SUCCESS: null,
+    LOAD_NOTES_FAIL: null
+});
